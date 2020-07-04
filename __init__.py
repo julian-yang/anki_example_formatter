932f5ca7acdc5ee3c698f5b383fb30171228f131
yang-julian <juliany@google.com>
2020-07-04 13:16:24 -0700

    open examples link via 'cmd/ctrl + /'

--- __init__.py
+++ __init__.py
@@ -3,15 +3,34 @@ from . import test
 # config = ConfigManager()
 # test.EditManager()
 import pprint
+import webbrowser
 
 # import the main window object (mw) from aqt
 from aqt import mw
 # import the "show info" tool from utils.py
-from aqt.utils import showInfo
+from aqt.utils import showInfo, openLink
 # import all of the Qt GUI library
 from aqt.qt import *
 from anki.hooks import addHook
+import sys
+from .lib.pinyin import pinyin
+from os.path import dirname, join
 
+sys.path.append(join(dirname(__file__), 'lib'))
+
+
+# input: the text from "Examples" field, this should be a list of example sentences.
+# returns: html table properly formatted.
+def format_examples(examples):
+    print('hello')
+
+def find_examples(editor):
+    if 'Hanzi' not in editor.note.keys():
+        showInfo(f"couldn't find Hanzi field! note fields: {editor.note.keys}")
+        return
+    hanzi = editor.note['Hanzi']
+    examples_link = f'https://tw.ichacha.net/zaoju/{hanzi}.html'
+    openLink(examples_link)
 
 def onToggle(editor):
     mid = str(editor.note.model()['id'])
@@ -25,8 +44,15 @@ def onToggle(editor):
         showInfo(f"couldn't find field! note fields: {editor.note.keys}")
         return
     hanzi = editor.note['Hanzi']
+    # examples_link = f'https://tw.ichacha.net/zaoju/{hanzi}.html'
+    # openLink(examples_link)
+    # showInfo(f'<a href=\"{examples_link}\">{examples_link}</a>', textFormat='rich')
+
     examples = editor.note['Examples']
-    showInfo(f'Hanzi: {hanzi}\nExamples:{pprint.pformat(examples)}')
+    # showInfo(f'Hanzi: {hanzi}\nExamples:{pprint.pformat(examples)}')
+    editor.note['Examples-front'] = 'hello world!'
+    editor.note.flush()
+    editor.setNote(editor.note)
 
 
 def setupButton(buttons, editor):
@@ -43,7 +69,17 @@ def setupButton(buttons, editor):
         toggleable=True,
         keys="Ctrl+;")
 
-    return buttons + [button]
+    find_examples_button = editor.addButton(
+        icon=None,
+        cmd='findExamples',
+        tip='Find Examples',
+        label='<b>Find Examples</b>',
+        func=find_examples,
+        id='findExamples',
+        toggleable=True,
+        keys="Ctrl+/")
+
+    return buttons + [button, find_examples_button]
 
 addHook('setupEditorButtons', setupButton)
 
